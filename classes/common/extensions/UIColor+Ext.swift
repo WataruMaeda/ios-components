b975639e93fa79746b76e3cb406cbfcd6208feef
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-04-18 21:22:10 -0700

    chore: update uicolor ext

--- classes/common/extensions/UIColor+Ext.swift
+++ classes/common/extensions/UIColor+Ext.swift
@@ -7,6 +7,31 @@ import UIKit
 
 extension UIColor {
   
-  static let keyColor = UIColor(red:0.204, green:0.290, blue:0.373, alpha:1.000)
+  static let keyColor = UIColor("#525c67")
   
+}
+
+extension UIColor {
+  
+  convenience init(_ hex: String) {
+    
+    let hex: String = (hex as NSString).trimmingCharacters(in: .whitespacesAndNewlines)
+    let scanner = Scanner(string: hex as String)
+    
+    if hex.hasPrefix("#") {
+      scanner.scanLocation = 1
+    }
+    var color: UInt32 = 0
+    scanner.scanHexInt32(&color)
+    
+    let mask = 0x000000FF
+    let r = Int(color >> 16) & mask
+    let g = Int(color >> 8) & mask
+    let b = Int(color) & mask
+    
+    let red = CGFloat(r) / 255.0
+    let green = CGFloat(g) / 255.0
+    let blue = CGFloat(b) / 255.0
+    self.init(red: red, green: green, blue: blue, alpha: 1)
+  }
 }
